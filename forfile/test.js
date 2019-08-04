acc82b94a653464243ba792cf91ec9f06a873625
dustin pfister <dustin.pfister@gmail.com>
2019-08-04 19:33:05 -0400

    deleted forfile

--- forfile/test.js
+++
@@ -1,6 +0,0 @@
-module.exports = (item, next ) => {
-    console.log('this is just a test of the forfile script feature.');
-    console.log('file found at: ' + item.path);
-    console.log('**********');
-    next();  
-};
